413c7deba57827643a1d67c48c4006dd8e432f4d
RibalTeeny <rnt09@mail.aub.edu>
2024-01-15 19:08:33 +0100

    Added upBlock of Unet and a middle block

--- DiffusionModel.py
+++ DiffusionModel.py
@@ -50,9 +50,43 @@ class DownBlock(nn.Module):
         print("block3:", x.shape)
         x = self.downSample(x)
         print("block3 downsampled:", x.shape)
+        return x
 
 class UpBlock():
-    pass
+    def __init__(self, out_channels):
+        self.upSample1 = nn.ConvTranspose2d(256, 256, (4,4), stride = (2,2), padding=(1,1))
+        self.block1 = Block(256, 128, kernel_size=(3,3), dim_mult=2, num_groups=32)
+        self.upSample2 = nn.ConvTranspose2d(128, 128, (4,4), stride = (2,2), padding=(1,1))
+        self.block2 = Block(128, 64, kernel_size=(3,3), dim_mult=2, num_groups=32)
+        self.upSample3 = nn.ConvTranspose2d(64, 64, (4,4), stride = (2,2), padding=(1,1))
+        self.block3 = Block(64, 16, kernel_size=(3,3), dim_mult=2, num_groups=8)
+        self.lastConv = nn.Conv2d(16, out_channels, (3,3), padding="same")
+
+    def forward(self, x):
+        print("x:", x.shape)
+        x = self.upSample1(x)
+        print("upSample1:", x.shape)
+        x = self.block1.forward(x)
+        print("block1:", x.shape)
+        x = self.upSample2(x)
+        print("upSample2:", x.shape)
+        x = self.block2.forward(x)
+        print("block2:", x.shape)
+        x = self.upSample3(x)
+        print("upSample3:", x.shape)
+        x = self.block3.forward(x)
+        print("block3:", x.shape)
+        x = self.lastConv(x)
+        print("last Layer:", x.shape)
+        return x
 
 class UNet(nn.Module):
-    pass
+    def __init__(self, in_channels, out_channels):
+        self.downPath = DownBlock(in_channels)
+        self.mid = Block(256, 256, (3,3), dim_mult=1, num_groups=32)
+        self.upPath = UpBlock(out_channels)
+
+    def forward(self, x):
+        x = self.downPath(x)
+        x = self.mid(x)
+        x = self.upPath(x)
